7fba98280b4b774c59f24e76964652c2278734d0
jhodara <jhodara@gmail.com>
2012-01-15 12:14:11 -0800

    paths

--- classes/utility/Paths.php
+++ classes/utility/Paths.php
@@ -2,10 +2,9 @@
 
 class Paths {
 	
-	const ROOT = '';
 	const SHORTURL = 'http://bouyreport.com/';
 	const URL = 'http://www.bouyreport.com/';
-	const LOCALURL = 'http://localhost:8888/br';
+	const LOCALURL = 'http://localhost:8888/';
 	const COOKIEDOMAIN = '.bouyreport.com';
 
 	public static function toUrl() {
@@ -34,15 +33,15 @@ class Paths {
 	}
 
 	public static function toIntro() {
-		return Paths::ROOT.'/';
+		return '/';
 	}	
 
 	public static function toUserHome() {
-		return Paths::ROOT.'/';
+		return '/';
 	}	
 
 	public static function toLogin($error = NULL) {
-		$url = Paths::ROOT.'/login.php';
+		$url = '/login.php';
 		if (isset($error)) {
 			$url .= '?error=' . $error;
 		}
@@ -50,11 +49,11 @@ class Paths {
 	}	
 
 	public static function toLogout() {
-		return Paths::ROOT.'/?logout=true';
+		return '/?logout=true';
 	}	
 
 	public static function toRegister($error = NULL) {
-		$url = Paths::ROOT.'/register.php';
+		$url = '/register.php';
 		if (isset($error)) {
 			$url .= '?error=' . $error;
 		} 
@@ -65,7 +64,7 @@ class Paths {
 		if (!isset($locationid)) {
 			return Paths::toLocations(null, true);
 		}
-		$url = Paths::ROOT.'/report.php?location='.$locationid;
+		$url = '/report.php?location='.$locationid;
 		if (isset($submitError)) {
 			$url .= '&error='.$submitError;
 		}
@@ -73,20 +72,20 @@ class Paths {
 	}
 
 	public static function toSinglePost($reportId) {
-		return Paths::ROOT.'/post.php?id='.$reportId;
+		return '/post.php?id='.$reportId;
 	}
 
 	public static function toEditPost($reportId) {
-		return Paths::ROOT.'/edit-post.php?id='.$reportId;
+		return '/edit-post.php?id='.$reportId;
 	}	
 
 	public static function toProfile($reporterId) {
-		return Paths::ROOT.'/profile.php?reporter='.$reporterId;
+		return '/profile.php?reporter='.$reporterId;
 	}
 
 	public static function toLocation($locationId = null) {
 		if (isset($locationId)) {
-			return Paths::ROOT.'/location.php?location='.$locationId;
+			return '/location.php?location='.$locationId;
 		} else { 
 			return Paths::toLocations();
 		}
@@ -94,7 +93,7 @@ class Paths {
 
 	public static function toLocations($reporterId = null, $toPost = FALSE) {
 
-		$url = Paths::ROOT.'/locations.php';
+		$url = '/locations.php';
 
 		if (isset($reporterId)) {
 			$url .= '?reporter=' . $reporterId;
@@ -107,7 +106,7 @@ class Paths {
 	}
 
 	public static function toReporters($locationId = null) {
-		$url = Paths::ROOT.'/reporters.php';
+		$url = '/reporters.php';
 		if (isset($locationId)) {
 			$url .= '?location=' . $locationId;
 		}
@@ -115,11 +114,11 @@ class Paths {
 	}
 
 	public static function toEditLocation($locationId) {
-		return Paths::ROOT.'/edit-location.php?location='.$locationId;
+		return '/edit-location.php?location='.$locationId;
 	}
 
 	public static function toSubmitLocation() {
-		return Paths::ROOT.'/add-location.php';
+		return '/add-location.php';
 	}
 
 	public static function toImageSrc($path, $isRemote = FALSE) {
@@ -141,26 +140,26 @@ class Paths {
 	}
 
 	public static function to404() {
-		return Paths::ROOT.'/404.php';
+		return '/404.php';
 	}
 		
 //-------------------------------------------------------------------------------//
 //-------------------------- CODE DIRECTORIES/FILES -----------------------------//
 	
 	public static function toAjax() {
-		return Paths::ROOT.'/ajax/';
+		return '/ajax/';
 	}
 		
 	public static function toJs() {
-		return Paths::ROOT.'/js/';
+		return '/js/';
 	}
 
 	public static function toCss() {
-		return Paths::ROOT.'/css/';
+		return '/css/';
 	}
 
 	public static function toImages() {
-		return Paths::ROOT.'/images/';
+		return '/images/';
 	}
 
 	public static function absoluteToImages() {
@@ -172,14 +171,14 @@ class Paths {
 		else {
 			$url = Paths::URL;
 		}
-		$url .= Paths::ROOT.'/images/';
+		$url .= '/images/';
 		return $url;
 	}	
 
 	public static function toMobileImageProcess() {
 		global $local_dev;
 
-		return Paths::toUrl() . Paths::ROOT.'/utility/mobile-image-process.php';
+		return Paths::toUrl() . '/utility/mobile-image-process.php';
 		//return 'http://192.168.1.105:8888/reporter/utility/mobile-image-process.php';
 
 	}
