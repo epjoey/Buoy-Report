4827a288bd3b111b840048cfbb3c1c6f98c091f1
jhodara <jhodara@gmail.com>
2012-01-15 14:02:15 -0800

    dns

--- classes/model/Report.php
+++ classes/model/Report.php
@@ -68,7 +68,7 @@ class Report {
 				$image = new SimpleImage();
 				$image->load($_FILES['upload']['tmp_name']);
 				$image->fitDimensions(1000,1000);
-				var_dump($_FILES['upload']['tmp_name']);
+				vardump($_FILES['upload']['tmp_name']);
 				if (!copy($_FILES['upload']['tmp_name'], $imagePath)) {
 					$this->submitError = 'file-save';
 				}				
