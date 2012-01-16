77051c9a4e4c306a892c6e26467c703827033df9
jhodara <jhodara@gmail.com>
2012-01-15 20:53:42 -0800

    mgcqts test

--- classes/pages/AddLocationPage.php
+++ classes/pages/AddLocationPage.php
@@ -1,7 +1,6 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/classes/pages/GeneralPage.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/classes/view/LocationList.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/utility/magicquotes.php';
 
 
 class AddLocationPage extends GeneralPage {
