6373396d6c7a91de7c420e1cc141f2f5a2e0176e
jhodara <jhodara@gmail.com>
2012-01-21 14:03:50 -0800

    print 'poo'

--- 404.php
+++ 404.php
@@ -4,6 +4,8 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/utility/helpers.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/classes/model/User.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/classes/pages/ErrorPage.php';
 
+print 'poo';
+
 $about = new ErrorPage();
 $about->loadData();
 $about->renderPage();
