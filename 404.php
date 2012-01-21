7a28721802c3bdf3c6ef7d5349c145f68241c5e8
jhodara <jhodara@gmail.com>
2012-01-21 14:07:31 -0800

    revert git change

--- 404.php
+++ 404.php
@@ -4,8 +4,6 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/utility/helpers.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/classes/model/User.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/classes/pages/ErrorPage.php';
 
-print 'poo';
-
 $about = new ErrorPage();
 $about->loadData();
 $about->renderPage();
