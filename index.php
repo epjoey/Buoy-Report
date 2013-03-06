ead6304e7602c56f02eefceab9e235de1b8f87c2
Joey <joey@eproject-inc.com>
2013-03-05 23:23:54 -0800

    hello world

--- index.php
+++ index.php
@@ -7,9 +7,6 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/classes/pages/HomePage.php';
 
 $user = new User;
 
-//intro page has no form handling. login form sent to login.php
-//if (/*!$user->isLoggedIn*/ isset($_REQUEST['intro']) && $_REQUEST['intro']) {
-
 if (!$user->isLoggedIn) {
 	$intro = new IntroPage;
 	$intro->loadData();
@@ -19,9 +16,5 @@ if (!$user->isLoggedIn) {
 
 $home = new HomePage;
 $home->loadData();
-$home->renderPage();	
-
-//scrape data from NOAA, use time to decipher wind & tide. pivate or public logs (added to the public log).
-//TIME, LOCATION, TIDE, WIND, DESCRIPTION
-
+$home->renderPage();
 ?>
