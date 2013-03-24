f55a870f9d031d8ab7b416f6f2eb02e5e95011ab
Joey <joey@eproject-inc.com>
2013-03-23 20:12:33 -0700

    base model

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -2,7 +2,7 @@
 class BaseModel {
 
 	//return false if any properties in props are either falsy or not set on $this
-	static function has($props) {
+	function has($props) {
 		$props = explode('and', $props);
 		foreach ($props as $prop) {
 			if (!$this->$prop) {
