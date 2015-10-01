3b335988c61f403d4b6f0ec691adbdcb13a5da02
epjoey <kanjoyatemp@Kanjoyas-MacBook-Air.local>
2015-09-30 21:49:15 -0700

    debug

--- view/templates/forms/ReportFormFields.php
+++ view/templates/forms/ReportFormFields.php
@@ -10,14 +10,14 @@ class ReportFormFields {
 				<option value="" />select</option>
 				<?
 				foreach ($waveHeights as $key=>$value) {
-					$selected = isset($preselected) && $preselected == $key ? "selected = 'selected'" : ''; 
+					$selected = isset($preselected) && $preselected == $key ? "selected = 'selected'" : '';
 					?>
 					<option value="<?=$key?>" <?=$selected?> /><?=$value[0] . '-' . $value[1]?></option>
 					<?
 				}
 				?>
 			</select>
-		</div>	
+		</div>
 		<?
 	}
 
@@ -29,32 +29,30 @@ class ReportFormFields {
 				<option value="" />select</option>
 				<?
 				foreach ($sublocations as $sublocation) {
-					$selected = isset($preselected) && $preselected == $sublocation->sl_id ? "selected = 'selected'" : ''; 
+					$selected = isset($preselected) && $preselected == $sublocation->sl_id ? "selected = 'selected'" : '';
 					?>
 					<option value="<?=$sublocation->sl_id?>" <?=$selected?> /><?=$sublocation->sl_name?></option>
 					<?
 				}
 				?>
 			</select>
-		</div>	
+		</div>
 		<?
 	}
 
 	public static function renderTimeSelect() {
-		?>				
+		?>
 		<div class="field time select-field required first">
 			<label for="time_offset">Time:</label>
 			<select name="time_offset" id="time-offset">
 				<option value="0">Now</option>
 				<?
-				for ($i=1; $i <= 48; $i++) { 
+				for ($i=1; $i <= 48; $i++) {
 					?>
 					<option value="-<?=$i?>"><?= $i . " hours ago" ?></option>
 					<?
 				}
-
 				?>
-				
 				<option value="older-date">Older...</option>
 			</select>
 			<input id="report-form-older-date" style="display:none" placeholder="mm/dd/yyyy hh:mm:ss" type="text" value="" name="time" />
@@ -62,14 +60,14 @@ class ReportFormFields {
 				(function(){
 					$('select#time-offset').change(function(event){
 						if ($(this).find(":selected").val() == 'older-date') {
-							$('#report-form-older-date').show();	
+							$('#report-form-older-date').show();
 						}
 					});
 				})();
 			</script>
 		</div>
 		<?
-	}	
+	}
 
 	public static function renderQualitySelect($currentQuality = null) {
 		?>
@@ -93,13 +91,13 @@ class ReportFormFields {
 				?>
 			</div>
 		</div>
-		<?		
+		<?
 	}
 
 	public static function renderImageInput($currImagePath, $needPicup) {
 		?>
 		<div class="field image last">
-			<label for="upload">Upload <?= $currImagePath ? "New" : "" ?> Image:</label> 
+			<label for="upload">Upload <?= $currImagePath ? "New" : "" ?> Image:</label>
 
 			<?
 			if ($currImagePath) {
@@ -108,13 +106,13 @@ class ReportFormFields {
 					?>
 					<span class="image-container">
 						<a href="<?=$image['src']?>" target="_blank"><image src="<?= $image['src'] ?>" width="<?=$image['width']?>" height="<?=$image['height']?>"/></a>
-						<label><input type="checkbox" name="delete-image" id="" value="true" /> Delete Image</label>	
+						<label><input type="checkbox" name="delete-image" id="" value="true" /> Delete Image</label>
 					</span>
-					<? 						
-				}				
+					<?
+				}
 			}
-			?>				
-		
+			?>
+
 			<input type="file" name="upload" id="upload" capture="camera">
 			<span id="mobile-image-name" class="mobile-note">
 				<?
@@ -126,6 +124,6 @@ class ReportFormFields {
 				?>
 			</span>
 		</div>
-		<?		
+		<?
 	}
 }
