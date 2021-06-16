9717e62f0168ad5fbf2b2f9aca8456c0d58f0213
Joey Hodara <joey@fareharbor.com>
2021-06-15 16:20:19 -1000

    css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const locations = require('../services/locations');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const rows = await locations.getMultiple(req.query.page);
+  res.render('index', { locations: rows });  
 });
 
 module.exports = router;
