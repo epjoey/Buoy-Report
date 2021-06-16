597a9ebd31b6af98677dd2bfc1e2404ee72450eb
Joey Hodara <joey@fareharbor.com>
2021-06-16 08:57:11 -1000

    Buoy page.

--- routes/locations.js
+++ routes/locations.js
@@ -2,15 +2,18 @@ const express = require('express');
 const router = express.Router();
 const locations = require('../services/locations');
 
-/* GET programming languages. */
 router.get('/', async function(req, res, next) {
-  try {
-    rows = await locations.getMultiple(req.query.page);
-    res.render('locations', { locations: rows });
-  } catch (err) {
-    console.error(`Error while getting locations `, err.message);
-    next(err);
-  }
+  const rows = await locations.getMultiple(req.query.page);
+  res.render('locations', { locations: rows });
+});
+
+router.get('/:locationId', async function(req, res, next) {
+  const location = await locations.getSingle(parseInt(req.params.locationId));
+  const buoys = await locations.getBuoys(location);
+  res.render('location', {
+    location: location,
+    buoys: buoys
+  });
 });
 
 module.exports = router;
--- services/locations.js
+++ services/locations.js
@@ -17,6 +17,34 @@ async function getMultiple(page = 1){
   }
 }
 
+async function getSingle(id){
+  let [rows, fields] = await db.query(
+    'SELECT * FROM `location` WHERE id = ?', 
+    [id]
+  );
+  let row = rows[0];
+  return row;
+}
+
+
+async function getBuoys(location, page = 1){
+  const LIMIT = 1000;
+  const offset = helper.getOffset(page, LIMIT);
+  let [rows, fields] = await db.query(
+    'SELECT * FROM `buoy` b JOIN `buoy_location` bl ON b.buoyid = bl.buoyid WHERE bl.locationid = ? LIMIT ?,?', 
+    [location.id, offset, LIMIT]
+  );
+  rows = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    rows,
+    meta
+  }
+}
+
 module.exports = {
-  getMultiple
+  getSingle,
+  getMultiple,
+  getBuoys
 }
