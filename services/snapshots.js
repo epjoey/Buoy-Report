f036955ee15f4b6a28e59dadc8c32b6f24b205b0
Joey Hodara <joey@fareharbor.com>
2021-07-20 15:19:09 -1000

    Forgot includes.

---
+++ services/snapshots.js
@@ -0,0 +1,60 @@
+const db = require('../db');
+const helper = require('../helper');
+
+
+async function forLocation(locationId, page = 1){
+  const LIMIT = 1000;
+  const offset = helper.getOffset(page, LIMIT);
+  let [rows, fields] = await db.query(
+    'SELECT r.id, r.text, r.quality, r.imagepath, \
+    r.obsdate, r.waveheight, \
+    u.id as reporterId, u.name as reporterName, u.email, \
+    l.id as locationId, l.locname, l.timezone \
+    FROM `report` r \
+    LEFT JOIN `reporter` u ON r.reporterid = u.id \
+    LEFT JOIN `location` l ON r.locationid = l.id \
+    WHERE r.locationid = ? AND r.public = 1 \
+    ORDER BY r.obsdate \
+    LIMIT ?,?;', 
+    [locationId, offset, LIMIT]
+  );
+  rows = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    rows,
+    meta
+  }
+}
+
+
+async function forReporter(reporterId, page = 1){
+  const LIMIT = 1000;
+  const offset = helper.getOffset(page, LIMIT);
+  let [rows, fields] = await db.query(
+    'SELECT r.id, r.text, r.quality, r.imagepath, \
+    r.obsdate, r.waveheight, \
+    u.id as reporterId, u.name as reporterName, u.email, \
+    l.id as locationId, l.locname, l.timezone \
+    FROM `report` r \
+    LEFT JOIN `reporter` u ON r.reporterid = u.id \
+    LEFT JOIN `location` l ON r.locationid = l.id \
+    WHERE r.reporterid = ? \
+    ORDER BY r.obsdate \
+    LIMIT ?,?;', 
+    [reporterId, offset, LIMIT]
+  );
+  rows = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    rows,
+    meta
+  }
+}
+
+
+module.exports = {
+  forLocation,
+  forReporter
+}
