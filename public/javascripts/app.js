5817a866641c483cbf2347f3fc47516da454eceb
Joey Hodara <joey@fareharbor.com>
2021-07-21 10:12:01 -1000

    Move filters into their own file.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,34 +1,5 @@
 (function() {
-  angular.module('app', ['directives', 'controllers'])
-
-  .filter('snapshotWaveHeight', function(){
-    // `snapshot.waveheight` is stored as an number representing a range.
-    // So 17.5 means the waves were 15-20.
-    var map = {
-      1.5: "1-2'",
-      2.5: "2-3'",
-      3.5: "3-4'",
-      5: "4-6'",
-      7: "6-8'",
-      9: "8-10'",
-      11: "10-12'",
-      13.5: "12-15'",
-      17.5: "15-20'",
-      25: "20-30'"
-    };
-    return function(waveHeight){
-      return map[waveHeight];
-    }
-  })
-
-  .filter('snapshotImagePath', function(){
-    return function(path){
-      if(path && !path.startsWith('http')){
-        return 'https://www.buoyreport.com/uploads/' + path;
-      }
-      return path;
-    }
-  })
+  angular.module('app', ['directives', 'controllers', 'filters'])
 
   .run([
     '$rootScope',
