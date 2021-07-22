cc7afb5b79c910929175438df65a068399fb1710
Joey Hodara <joey@fareharbor.com>
2021-07-21 14:58:28 -1000

    Try to fix auth0.

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const session = {
   saveUninitialized: false
 };
 
-if(app.get("env") === "production"){
+if(process.env.NODE_ENV === "production"){
   // Serve secure cookies, requires HTTPS
   session.cookie.secure = true;
 }
