efc64b7c2c69a980908498a82d2a2607c4226a4f
Joey Hodara <joey@fareharbor.com>
2021-08-14 17:51:19 -1000

    Emoticon sql :).

--- migrations/migration_8.sql
+++ migrations/migration_8.sql
@@ -3,3 +3,9 @@ CREATE TABLE `favorites` (
   `locationid` int(11) NOT NULL,
   PRIMARY KEY (`email`,`locationid`)
 );
+
+ALTER DATABASE br CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+ALTER TABLE report CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE report MODIFY text TEXT CHARSET utf8mb4;
