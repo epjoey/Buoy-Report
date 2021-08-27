fddadb01d9e98b4229bfe355dcd291a08f3eb8fd
Joey Hodara <joey@fareharbor.com>
2021-08-27 08:12:47 -1000

    webpack.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /**
  * Required External Modules
  */
@@ -9,6 +11,8 @@ const Auth0Strategy = require("passport-auth0");
 const logger = require('morgan');
 const cookieSession = require('cookie-session')
 const cookieParser = require('cookie-parser');
+var debug = require('debug')('buoy-report:server');
+var http = require('http');
 
 require("dotenv").config(); // loads the vars from the .env file into process.env
 const ADMIN_EMAIL = "jhodara@gmail.com";
@@ -25,7 +29,6 @@ const apiRouter = require('./routes/api');
  * App Variables
  */
 const app = express();
-// const port = process.env.PORT || "3000";
 
 /**
  * Session Configuration (New!)
@@ -77,7 +80,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'static')));
 app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
@@ -130,12 +134,71 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
+  // render th error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+
 /**
- * Note: Server Activation happens in bin/www
+ * Get port from environment and store in Express.
  */
-module.exports = app;
+
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
