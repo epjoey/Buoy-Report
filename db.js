8b2c2ed987039df2f20e37e632d62950bfb6d0ae
joey hodara <jhodara@gmail.com>
2025-11-30 20:28:28 -1000

    changes to docs and db config

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
+  user: process.env.DB_USER || 'bruser',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'br',
   port: 3306,
